b140395a9255135aaf2aa43a2bff0641efe01dae
Jayadasan S <jayadasanjai@gmail.com>
2026-01-05 17:45:53 +0530

    Create ReverseString.java

---
+++ Day1/ReverseString.java
@@ -0,0 +1,16 @@
+//Question-https://leetcode.com/problems/reverse-string/description/
+class Solution {
+    public void reverseString(char[] s) {
+        int left =0;
+        int ryt =s.length-1;
+        while(left < ryt)
+        {
+            char temp = s[left];
+            s[left]=s[ryt];
+            s[ryt]=temp;
+            left ++;
+            ryt--;
+        }
+        
+    }
+}
