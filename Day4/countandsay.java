d9e2d4a2f877e17e5cf90a8f312513eb249c5b11
Jayadasan S <jayadasanjai@gmail.com>
2026-01-08 18:30:57 +0530

    Day4 completed

---
+++ Day4/countandsay.java
@@ -0,0 +1,27 @@
+package Day4;
+
+public class countandsay {
+
+    public String countAndSay(int n) {
+        String result = "1";  
+        for (int i = 2; i <= n; i++) {   
+            String current = "";
+            int count = 1;
+
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;  
+                } else {
+                    current = current + count + result.charAt(j - 1);
+                    count = 1; 
+                }
+            }
+             current = current + count + result.charAt(result.length() - 1);
+            result = current;  
+        }
+        return result;
+        
+    }
+}
+    
+
